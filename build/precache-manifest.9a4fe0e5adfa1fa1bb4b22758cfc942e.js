37d202bd4248c963989f6955918bb4f1a48f97e3
Choxy <choxypop@gmail.com>
2025-11-09 23:03:14 +0000

    build

---
+++ build/precache-manifest.9a4fe0e5adfa1fa1bb4b22758cfc942e.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc42ba32e2321ed44d6351f0e3418f6f",
+    "url": "/winOS/public/index.html"
+  },
+  {
+    "revision": "46043f1f0f5dd3e37057",
+    "url": "/winOS/public/static/css/main.950adebf.chunk.css"
+  },
+  {
+    "revision": "1dfde960d127e18beb81",
+    "url": "/winOS/public/static/js/0.6b7cb01a.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/winOS/public/static/js/0.6b7cb01a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1341f5fb34f14e4fef7",
+    "url": "/winOS/public/static/js/1.56c4bd55.chunk.js"
+  },
+  {
+    "revision": "e6d0655e8878bd1aa51c",
+    "url": "/winOS/public/static/js/10.13891048.chunk.js"
+  },
+  {
+    "revision": "7b6ef9eef916c8929fc7",
+    "url": "/winOS/public/static/js/11.0a4a0411.chunk.js"
+  },
+  {
+    "revision": "b04dbdd4afaa582b301e",
+    "url": "/winOS/public/static/js/12.ae286b9d.chunk.js"
+  },
+  {
+    "revision": "455f9f3ae849b1b7c9d5b5f2d351830a",
+    "url": "/winOS/public/static/js/12.ae286b9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c8ac7745487ffb0bd33",
+    "url": "/winOS/public/static/js/2.57d9fc4b.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/winOS/public/static/js/2.57d9fc4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15ea87853f5a52b66887",
+    "url": "/winOS/public/static/js/5.0f7bc9cd.chunk.js"
+  },
+  {
+    "revision": "06856a34b02b6674263c",
+    "url": "/winOS/public/static/js/6.8f5d5a50.chunk.js"
+  },
+  {
+    "revision": "13e2f2ead3395760386c9d60e2bb9e15",
+    "url": "/winOS/public/static/js/6.8f5d5a50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9681eeb4025e431171d3",
+    "url": "/winOS/public/static/js/7.55d34aee.chunk.js"
+  },
+  {
+    "revision": "a3fb4b66faaf8fd95167",
+    "url": "/winOS/public/static/js/8.93066c19.chunk.js"
+  },
+  {
+    "revision": "0e6ade2d74d26d7f02a5",
+    "url": "/winOS/public/static/js/9.c4443709.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/winOS/public/static/js/9.c4443709.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46043f1f0f5dd3e37057",
+    "url": "/winOS/public/static/js/main.0ffccd9e.chunk.js"
+  },
+  {
+    "revision": "2cc1ba957b93f6247f58",
+    "url": "/winOS/public/static/js/runtime-main.d94c82cf.js"
+  },
+  {
+    "revision": "657571447d240b5c1127f06153c3b766",
+    "url": "/winOS/public/static/media/bliss.65757144.jpg"
+  },
+  {
+    "revision": "5eb0d40d417425bb2dfa2ed6a7b7ab0b",
+    "url": "/winOS/public/static/media/profile.5eb0d40d.png"
+  },
+  {
+    "revision": "6ea05914e55d941699f46ae5cac6f3a3",
+    "url": "/winOS/public/static/media/startpanel-moreprograms-hover.6ea05914.png"
+  },
+  {
+    "revision": "cdbf7a9017cd09b362b20a7baee206f3",
+    "url": "/winOS/public/static/media/tray-expand.cdbf7a90.png"
+  }
+]);
